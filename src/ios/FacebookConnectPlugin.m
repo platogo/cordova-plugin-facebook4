e99558f7bf38fa4d2743952c6f1e0aad2dcdf651
Athorcis <Athorcis@users.noreply.github.com>
2023-03-10 09:23:58 +0100

    fix(iOS): Prefixing swizzling logic to avoid conflicts (#5)

--- src/ios/FacebookConnectPlugin.m
+++ src/ios/FacebookConnectPlugin.m
@@ -1035,7 +1035,7 @@
 
 void FBMethodSwizzle(Class c, SEL originalSelector) {
     NSString *selectorString = NSStringFromSelector(originalSelector);
-    SEL newSelector = NSSelectorFromString([@"swizzled_" stringByAppendingString:selectorString]);
+    SEL newSelector = NSSelectorFromString([@"FacebookConnectPlugin_" stringByAppendingString:selectorString]);
     SEL noopSelector = NSSelectorFromString([@"noop_" stringByAppendingString:selectorString]);
     Method originalMethod, newMethod, noop;
     originalMethod = class_getInstanceMethod(c, originalSelector);
@@ -1053,7 +1053,7 @@ void FBMethodSwizzle(Class c, SEL originalSelector) {
     FBMethodSwizzle([self class], @selector(application:openURL:options:));
 }
 
-- (BOOL)swizzled_application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+- (BOOL)FacebookConnectPlugin_application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
     if (!url) {
         return NO;
     }
@@ -1066,7 +1066,7 @@ void FBMethodSwizzle(Class c, SEL originalSelector) {
     NSLog(@"FB handle url using application:openURL:options: %@", url);
 
     // Call existing method
-    return [self swizzled_application:application openURL:url options:options];
+    return [self FacebookConnectPlugin_application:application openURL:url options:options];
 }
 
 - (BOOL)noop_application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
